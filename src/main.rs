e2c5e25d21a92036d532ea624146e5f5e971d2ed
Sam Miller <sm1215@gmail.com>
2019-12-22 02:31:24 -0500

    read from csv file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 use std::string::String;
 use std::fmt;
+use std::fs;
+use std::num;
 
 struct Item {
     name: String,
@@ -13,27 +15,61 @@ impl fmt::Display for Item {
     }
 }
 
+struct Command {
+    name: String,
+    description: String,
+}
+
 // 1: create a list of weapons / armors
 // 2: user inputs materials to break down
 // 3: search for item, add to list to break down
 // 4: total up materials value list
 // 5: filter armory list based on what user can afford
 fn main() {
-    let armory = [
-        Item { name: String::from("Padded"), value: 5 },
-        Item { name: String::from("Leather"), value: 10 },
-        Item { name: String::from("Studded Leather"), value: 45 },
-        Item { name: String::from("Hide"), value: 10 },
-        Item { name: String::from("Chain Shirt"), value: 50 },
-        Item { name: String::from("Scale Mail"), value: 50 },
-        Item { name: String::from("Breastplate"), value: 400 },
-        Item { name: String::from("Half plate"), value: 750 },
-        Item { name: String::from("Ring mail"), value: 30 },
-        Item { name: String::from("Chain mail"), value: 75 },
-        Item { name: String::from("Splint"), value: 200 },
-        Item { name: String::from("Plate"), value: 1500 },
-        Item { name: String::from("Shield"), value: 10 },
-    ];
+    let mut armory: Vec<Item> = Vec::new();
+    let contents = fs::read_to_string("armory.csv")
+        .expect("Something went wrong loading the armory.");
+
+    for mut line in contents.lines() {
+        line = line.trim();
+        let entry: Vec<&str> = line.split(",").collect();
+
+        let name = entry[0].to_string();
+
+        let value: Result<u32, num::ParseIntError> = entry[1].parse();
+        let value: u32 = match value {
+            Ok(val) => val,
+            Err(err) => { continue },
+        };
+
+        let item = Item {
+            name,
+            value,
+        };
+
+        armory.push(item);
+    }
+
+    // todo: add more commands
+    // let commands = [
+    //     Command {
+    //         name: String::from("list armory"),
+    //         description: String::from("List the contents of the armory"),
+    //     },
+    //     Command {
+    //         name: String::from("search"),
+    //         description: String::from("Search for an item name in the armory"),
+    //     },
+    //     Command {
+    //         name: String::from("add <item_name>"),
+    //         description: String::from("Adds an item from the armory to your components list"),
+    //     },
+    //     Command {
+    //         name: String::from(""),
+    //         description: String::from(""),
+    //     },
+    // ];
+
     let mut materials: Vec<&Item> = Vec::new();
     let mut total_value: u32 = 0;
 
@@ -46,14 +82,19 @@ fn main() {
         
         input = input.trim().to_lowercase();
 
-        println!("{}", input);
+        match input.as_ref() {
+            "list" => print_list(&armory),
+            "forge" => break,
+            _ => println!("try again"),
+        }
+
         if input == "forge" {
             break;
         }
 
         let mut found = false;
         for item in &armory {
-            if command == item.name.to_lowercase() {
+            if input == item.name.to_lowercase() {
                 found = true;
                 println!("Adding {}", item.name);
                 materials.push(item);
@@ -61,7 +102,7 @@ fn main() {
         }
 
         if found == false {
-            println!("Item {} not found", command);
+            println!("Item {} not found", input);
         }
 
         total_value = materials.iter().fold(0, |acc, item| acc + item.value);
@@ -72,4 +113,11 @@ fn main() {
     for item in armory.iter().filter(|item| item.value <= total_value) {
         println!("{}", item);
     }
+}
+
+fn print_list(list: &[Item]) -> () {
+    for item in list {
+        println!("{}", item);
+    }
+    ()
 }
